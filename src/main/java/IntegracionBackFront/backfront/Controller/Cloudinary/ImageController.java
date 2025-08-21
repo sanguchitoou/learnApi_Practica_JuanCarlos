2067233a472bb17d19ffe69c6864079601b69c2a
sanguchito <2017razor2017@gmail.com>
2025-08-21 14:04:49 -0600

    Implementación de IMÁGENES CON CLOUDINARY - Raíz

--- src/main/java/IntegracionBackFront/backfront/Controller/Cloudinary/ImageController.java
+++ src/main/java/IntegracionBackFront/backfront/Controller/Cloudinary/ImageController.java
@@ -1,10 +1,45 @@
 package IntegracionBackFront.backfront.Controller.Cloudinary;
 
+import IntegracionBackFront.backfront.Services.Cloudinary.CloudinaryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.channels.MulticastChannel;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/image")
+@Validated
 public class ImageController {
+    //Inyectamos servicio de Cloudinary
+    @Autowired
+    private final CloudinaryService objCloudService;
 
+    //Constructor de CloudinaryService
+    public ImageController(CloudinaryService objCloudService) {
+        this.objCloudService = objCloudService;
+    }
+
+    //Método que permite CARGAR LA IMÁGEN (POST) con respuesta a la entidad
+    @PostMapping("/postImage")
+    public ResponseEntity<?> postImage(@RequestParam("image") MultipartFile file){
+        try{
+            //Llamamos al SERVICIO para subir la imagen y obtenemos la URL de la misma
+            String imageUrl = objCloudService.uploadImage(file);
+            //Se debe de llamar SI O SI esta variable URL, a la hora de llamar al FRONTEND
+            return ResponseEntity.ok(Map.of(
+                    "message", "Imagen subida exitosamente",
+                    "url", imageUrl
+            ));
+        } catch (IOException e) {
+            return ResponseEntity.internalServerError().body("Error al subir la imágen, error: " + e);
+        }
+    }
 }
--- src/main/java/IntegracionBackFront/backfront/Services/Cloudinary/CloudinaryService.java
+++ src/main/java/IntegracionBackFront/backfront/Services/Cloudinary/CloudinaryService.java
@@ -1,11 +1,13 @@
 package IntegracionBackFront.backfront.Services.Cloudinary;
 
 import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Map;
 
 @Service
 public class CloudinaryService {
@@ -25,39 +27,47 @@ public class CloudinaryService {
     }
 
     //Creación del método que cargará el cometido (imágenes, PDFS, etc.)
-    //MultipartFile sirve para subir CUALQUIER ARCHIVO
+    //MultipartFile sirve para subir CUALQUIER ARCHIVO, subida de imágenes a la raíz de CLOUDINARY (Sin carpetas)
     public String uploadImage(MultipartFile file) throws IOException {
         validateImage(file);
-        return null;
+        Map<?, ?> updloadResult = cloudinary.uploader().upload(file.getBytes(), ObjectUtils.asMap(
+                "resource_type", "auto",
+                "quality", "auto:good"
+        ));
+        //Retornamos la imágen subida a la RAÍZ de Cloudinary
+        return (String) updloadResult.get("secure_url");
     }
 
     //Creación del MÉTODO que validará la subida del archivo
     private void validateImage(MultipartFile file) {
         //Verificamos si el archivo está vacío
-        if(file == null || file.isEmpty()){
-            throw new IllegalArgumentException("El archivo está vacío");
+        if (file == null || file.isEmpty()) {
+            throw new IllegalArgumentException("El archivo no puede estar vacío");
         }
 
         //Verificamos si el tamaño excede el límite permitidos
-        if(file.getSize() > MAX_FILE_SIZE){
+        if (file.getSize() > MAX_FILE_SIZE) {
             throw new IllegalArgumentException("El tamaño del archivo no debe ser mayor a 5MB");
         }
 
         //Verificamos la extensión del archivo
         String originalFileName = file.getOriginalFilename();
-        if(originalFileName == null){
+        if (originalFileName == null) {
             throw new IllegalArgumentException("El nombre del archivo es inválido");
         }
 
         //Extraemos y validamos la extensión del archivo
         String validExtension = originalFileName.substring(originalFileName.lastIndexOf(".")).toLowerCase();
-        if(!Arrays.asList(ALLOWED_EXTENSIONS).contains(validExtension)){
+        if (!Arrays.asList(ALLOWED_EXTENSIONS).contains(validExtension)) {
             throw new IllegalArgumentException("Solo se permiten archivos .jpg, .jpeg, .png");
         }
 
         //Verificamos que el tipo MIME sea una imágen
-        if(!file.getContentType().startsWith("image/")){
+        if (!file.getContentType().startsWith("image/")) {
             throw new IllegalArgumentException("El archivo debe ser una imágen válida");
         }
     }
+
+    //Método que permie subir imágenes EN CARPETAS ESPECÍFICAS dentro de Cloudinary
+
 }
